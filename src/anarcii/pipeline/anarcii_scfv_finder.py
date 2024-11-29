b11e6a49be255c2c9a51e392b24d2fdb5627aa8d
ALGW71 <GreenshieldsWatsonAL@gmail.com>
2024-11-29 12:02:49 +0000

    Update with new  examples and allow for SCFV format.

---
+++ src/anarcii/pipeline/anarcii_scfv_finder.py
@@ -0,0 +1,27 @@
+from anarcii.input_data_processing.sequences import SequenceProcessor
+from anarcii.inference.model_runner import ModelRunner
+from anarcii.inference.window_selector import WindowFinder
+
+import json
+
+
+def batch_process(ls, model, window_model, verbose, txt_file):
+
+    counter = 1
+    for chunk in ls:
+        print(f"\nChunk: {counter} of {len(ls)}.")
+        sequences = SequenceProcessor(chunk, model, window_model, verbose)
+        processed_seqs = sequences.process_sequences()
+
+        # Process and write to the temp file
+        numbered_seqs=model(processed_seqs)
+
+        with open(txt_file, "a") as file:
+            for item in numbered_seqs:
+                file.write(repr(item) + "\n")
+
+        # Free up space
+        del sequences, processed_seqs, numbered_seqs
+        counter += 1
+
+    return []
