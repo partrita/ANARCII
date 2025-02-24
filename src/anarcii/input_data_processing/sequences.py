10ef0ed2b6031fa391fd36770db390bfe943c686
Ben Williams <benjaminhwilliams@users.noreply.github.com>
2025-02-24 18:06:32 +0000

    Only perform a single loop over long_seqs

--- src/anarcii/input_data_processing/sequences.py
+++ src/anarcii/input_data_processing/sequences.py
@@ -6,7 +6,10 @@ from .sequences_utils import find_scfvs, pick_window, split_seq
 
 # from anarcii.pipeline.anarcii_constants import n_jump
 
-cwc = re.compile(r".{,40}(?=C.{5,20}W.{50,80}C).{,160}")
+# A regex pattern to match no more than 200 residues, containing a 'CWC' pattern
+# (cysteine followed by 5–20 residues followed by a tryptophan followed by 50–80
+# residues followed by another cysteine) starting no later than the 41st residue.
+cwc_pattern = re.compile(r".{,40}(?=C.{5,20}W.{50,80}C).{,160}")
 
 
 class SequenceProcessor:
@@ -83,58 +86,45 @@ class SequenceProcessor:
             n_jump = 3
             long_seqs = {key: seq for key, seq in self.seqs.items() if len(seq) > 200}
 
-            if long_seqs:
-                if self.verbose:
-                    print(
-                        f"\n {len(long_seqs)} Long sequences detected - "
-                        "running in sliding window. This is slow."
-                    )
+            if long_seqs and self.verbose:
+                print(
+                    f"\n {len(long_seqs)} Long sequences detected - running in sliding "
+                    "window. This is slow."
+                )
 
-                for long_key in list(long_seqs.keys()):  # Iterate over a copy of keys
-                    # first try a simple regex to look for cwc
-                    cwcs = list(cwc.finditer(long_seqs[long_key]))
-                    cwc_matches = [m.group() for m in cwcs]
+            for key, sequence in long_seqs.items():
+                # first try a simple regex to look for cwc
+                cwc_matches = list(cwc_pattern.finditer(sequence))
+                cwc_strings = [m.group() for m in cwc_matches]
 
+                if cwc_matches:
                     if len(cwc_matches) > 1:
-                        cwc_winner = pick_window(cwc_matches, self.window_model)
+                        cwc_winner = pick_window(cwc_strings, self.window_model)
+                    else:
+                        cwc_winner = 0
 
-                        # Append the offset
-                        self.offsets[long_key] = cwcs[cwc_winner].start()
-                        self.seqs[long_key] = cwc_matches[cwc_winner]
+                    # Append the start offset
+                    self.offsets[key] = cwc_matches[cwc_winner].start()
+                    # Replace the input sequence
+                    self.seqs[key] = cwc_strings[cwc_winner]
 
-                        # remove from long seqs dict
-                        del long_seqs[long_key]
+                else:
+                    # If no cwc pattern is found, use the sliding window approach.
+                    # Split the sequence into 90-residue chunks and pick the best.
+                    windows = split_seq(sequence, n_jump=n_jump)
+                    best_window = pick_window(windows, model=self.window_model)
 
-                    elif cwc_matches:
-                        self.offsets[long_key] = cwcs[0].start()
-                        self.seqs[long_key] = cwc_matches[0]
-                        # remove from long seqs dict
-                        del long_seqs[long_key]
+                    # Ensures start_index is at least 0.
+                    start_index = max((best_window * n_jump) - 40, 0)
+                    end_index = (best_window * n_jump) + 160
 
-                # Splits the seqeucne in 90 length chunks
-                split_dict = {
-                    key: split_seq(seq, n_jump=n_jump) for key, seq in long_seqs.items()
-                }
-
-                res_dict = {
-                    key: pick_window(ls, self.window_model)
-                    for key, ls in split_dict.items()
-                }
-
-                # magic number alert...
-                for key, value in res_dict.items():
-                    start_index = max(
-                        (value * n_jump) - 40, 0
-                    )  # Ensures start_index is at least 0
-                    end_index = (value * n_jump) + 160
-
+                    # Append the start offset
                     self.offsets[key] = start_index
+                    # Replace the input sequence
+                    self.seqs[key] = sequence[start_index:end_index]
 
-                    # Slice the sequence
-                    self.seqs[key] = long_seqs[key][start_index:end_index]
-
-                if self.verbose:
-                    print("Max probability windows selected.\n")
+            if self.verbose:
+                print("Max probability windows selected.\n")
 
     def _convert_to_tuple_list(self):
         """
