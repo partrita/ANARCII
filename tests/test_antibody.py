ebe2e63fc03967b90093e250a5f9ecd9c4179573
ALGW71 <GreenshieldsWatsonAL@gmail.com>
2025-02-27 18:23:10 +0000

    Alt number scheme test is working - onto next check.

--- tests/test_antibody.py
+++ tests/test_antibody.py
@@ -25,10 +25,42 @@ def test_files_are_identical(anarcii_model, tmp_path):
     anarcii_model.to_text(tmp_path / txt_file)
     anarcii_model.to_json(tmp_path / json_file)
 
-    for expected, test in zip(
-        ["data/antibody_expected_1.txt", "data/antibody_expected_1.json"],
-        [txt_file, json_file],
-    ):
+    # Alternate number schemes
+    json_chothia_file = tmp_path / "antibody_chothia_test_1.json"
+    anarcii_model.to_scheme("chothia")
+    anarcii_model.to_json(tmp_path / json_chothia_file)
+
+    json_imgt_file = tmp_path / "antibody_imgt_test_1.json"
+    anarcii_model.to_scheme("imgt")
+    anarcii_model.to_json(tmp_path / json_imgt_file)
+
+    json_martin_file = tmp_path / "antibody_martin_test_1.json"
+    anarcii_model.to_scheme("martin")
+    anarcii_model.to_json(tmp_path / json_martin_file)
+
+    json_kabat_file = tmp_path / "antibody_kabat_test_1.json"
+    anarcii_model.to_scheme("kabat")
+    anarcii_model.to_json(tmp_path / json_kabat_file)
+
+    expected_files = [
+        "data/antibody_expected_1.txt",
+        "data/antibody_expected_1.json",
+        "data/antibody_chothia_expected_1.json",
+        "data/antibody_imgt_expected_1.json",
+        "data/antibody_kabat_expected_1.json",
+        "data/antibody_martin_expected_1.json",
+    ]
+
+    test_files = [
+        txt_file,
+        json_file,
+        json_chothia_file,
+        json_imgt_file,
+        json_kabat_file,
+        json_martin_file,
+    ]
+
+    for expected, test in zip(expected_files, test_files):
         with open(expected) as f1, open(test) as f2:
             content1 = f1.read().strip()
             content2 = f2.read().strip()
