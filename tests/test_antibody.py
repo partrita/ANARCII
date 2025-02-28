f8318241b162d01b0d33a0c9b7087bba92774cfb
AlexGW <37866978+ALGW71@users.noreply.github.com>
2025-02-28 14:48:16 +0000

    Apply suggestions from code review - improve test code with parameterisation
    Co-authored-by: Ben Williams <benjaminhwilliams@users.noreply.github.com>

--- tests/test_antibody.py
+++ tests/test_antibody.py
@@ -20,34 +20,32 @@ def anarcii_model():
 
 @pytest.mark.parametrize("scheme", ["default", "chothia", "imgt", "martin", "kabat", "aho"])
 def test_files_are_identical(anarcii_model, tmp_path):
-    schemes = ["default", "chothia", "imgt", "martin", "kabat", "aho"]
 
     expected_file_templates = {
         "txt": "data/antibody{suffix}_expected_1.txt",
         "json": "data/antibody{suffix}_expected_1.json",
     }
 
-    for scheme in schemes:
-        suffix = "" if scheme == "default" else f"_{scheme}"
+    suffix = "" if scheme == "default" else f"_{scheme}"
 
-        # Switch scheme if necessary (skip for "default")
-        if scheme != "default":
-            anarcii_model.to_scheme(scheme)
+    # Switch scheme if necessary (skip for "default")
+    if scheme != "default":
+        anarcii_model.to_scheme(scheme)
 
-        # Generate and check both text and json files
-        for filetype in ["txt", "json"]:
-            test_file = tmp_path / f"antibody{suffix}_test_1.{filetype}"
-            expected_file = expected_file_templates[filetype].format(suffix=suffix)
+    # Generate and check both text and json files
+    for filetype in ["txt", "json"]:
+        test_file = tmp_path / f"antibody{suffix}_test_1.{filetype}"
+        expected_file = expected_file_templates[filetype].format(suffix=suffix)
 
-            if filetype == "txt":
-                anarcii_model.to_text(test_file)
-            else:
-                anarcii_model.to_json(test_file)
+        if filetype == "txt":
+            anarcii_model.to_text(test_file)
+        else:
+            anarcii_model.to_json(test_file)
 
-            with open(expected_file) as f1, open(test_file) as f2:
-                content1 = f1.read().strip()
-                content2 = f2.read().strip()
+        with open(expected_file) as f1, open(test_file) as f2:
+            content1 = f1.read().strip()
+            content2 = f2.read().strip()
 
-            assert content1 == content2, (
-                f"Files {expected_file} and {test_file}" + " are different!"
-            )
+        assert content1 == content2, (
+            f"Files {expected_file} and {test_file}" + " are different!"
+        )
