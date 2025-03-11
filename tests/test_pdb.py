38a9b325daccd268d5e8f742abaa3ca2fb8f36fe
AlexGW <37866978+ALGW71@users.noreply.github.com>
2025-03-11 17:02:08 +0000

    Apply suggestions from code review
    Co-authored-by: Ben Williams <benjaminhwilliams@users.noreply.github.com>

--- tests/test_pdb.py
+++ tests/test_pdb.py
@@ -15,26 +15,22 @@ def anarcii_model():
         mode="accuracy",
         verbose=True,
     )
-    model.number("data/raw_data/1kb5.pdb")
-    model.number("data/raw_data/8kdm.pdb")
+    model.number(str(tmp_path / "1kb5.pdb"))
+    model.number(str(tmp_path / "8kdm.pdb"))
+    
+    return tmp_path
 
 
-def test_files_are_identical(anarcii_model, tmp_path):
-    shutil.move("data/raw_data/1kb5_anarcii.pdb", tmp_path / "1kb5_anarcii.pdb")
+@pytest.mark.parametrize("filename", ["1kb5_anarcii.pdb", "8kdm_anarcii.pdb"])
+def test_files_are_identical(anarcii_model, filename):
+    """Generate and check both text and json files."""
+    expected_file = f"data/expected_data/{filename}"
+    test_file = anarcii_model / filename
+    
+    with open(expected_file) as f1, open(test_file) as f2:
+        content1 = f1.read().strip()
+        content2 = f2.read().strip()
 
-    shutil.move("data/raw_data/8kdm_anarcii.pdb", tmp_path / "8kdm_anarcii.pdb")
-
-    expected_files = ["1kb5_anarcii.pdb", "8kdm_anarcii.pdb"]
-
-    # Generate and check both text and json files
-    for file in expected_files:
-        test_file = tmp_path / file
-        expected_file = "data/expected_data/" + file
-
-        with open(expected_file) as f1, open(test_file) as f2:
-            content1 = f1.read().strip()
-            content2 = f2.read().strip()
-
-        assert content1 == content2, (
-            f"Files {expected_file} and {test_file}" + " are different!"
-        )
+    assert content1 == content2, (
+        f"Files {expected_file} and {test_file} are different!"
+    )
