9fd523e6ffb1f738708d8bc18c431862e4214270
ALGW71 <GreenshieldsWatsonAL@gmail.com>
2025-03-20 22:19:09 +0000

    Fixed the immediate issue - seq type was not being used correctly for normal seqs versus the PDB.

--- src/anarcii/pipeline/__init__.py
+++ src/anarcii/pipeline/__init__.py
@@ -168,11 +168,18 @@ class Anarcii:
                 verbose=self.verbose,
             )
 
-        # PDB files can be run directly, bypassing classifii at this stage.
-        # They will be classified into ab/tcrs by an inner model which takes a list of
-        #  seqs read from a PDB file (these can now pass through the Classifii code).
-        else:
+        elif ".pdb" in seqs or ".mmcif" in seqs:
+            # PDB files can be run directly, bypassing classifii at this stage.
+            # They will be classified into ab/tcrs by an inner model which takes list of
+            #  seqs read from a PDB file (these can pass through the Classifii code).
             self._last_numbered_output = self.number_with_type(seqs, "antibody")
+            return convert_output(
+                ls=self._last_numbered_output,
+                format=self.output_format,
+                verbose=self.verbose,
+            )
+        else:
+            self._last_numbered_output = self.number_with_type(seqs, self.seq_type)
             return convert_output(
                 ls=self._last_numbered_output,
                 format=self.output_format,
