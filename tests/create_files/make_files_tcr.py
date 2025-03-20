9fa61ed1cadcb057a5a027298aff44a5696d1cab
ALGW71 <GreenshieldsWatsonAL@gmail.com>
2025-03-20 22:33:09 +0000

    Added the TCR test.

--- tests/create_files/make_files_tcr.py
+++ tests/create_files/make_files_tcr.py
@@ -1,27 +1,27 @@
-# from anarcii import Anarcii
+from anarcii import Anarcii
 
-# # FILE GENERATION
-# model = Anarcii(
-#     seq_type="tcr",
-#     batch_size=64,
-#     cpu=False,
-#     ncpu=12,
-#     mode="speed",
-#     verbose=False,
-# )
-# model.number("../data/stcrdab_filtered.fa")
+# FILE GENERATION
+model = Anarcii(
+    seq_type="tcr",
+    batch_size=64,
+    cpu=False,
+    ncpu=12,
+    mode="accuracy",
+    verbose=False,
+)
+model.number("../data/raw_data/tcr_check.fa")
 
-# model.to_text("../data/tcr_expected_1.txt")
-# model.to_json("../data/tcr_expected_1.json")
+model.to_text("../data/expected_data/tcr_expected_1.txt")
+model.to_json("../data/expected_data/tcr_expected_1.json")
 
-# for scheme in [
-#     "kabat",
-#     "chothia",
-#     "martin",
-#     "imgt",
-#     #    "aho" # Cannot number TCRs in AHo...
-# ]:
-#     out = model.to_scheme(f"{scheme}")
+for scheme in [
+    "kabat",
+    "chothia",
+    "martin",
+    "imgt",
+    #    "aho" # need to fix Aho TCR
+]:
+    out = model.to_scheme(f"{scheme}")
 
-#     model.to_json(f"../data/tcr_{scheme}_expected_1.json")
-#     model.to_text(f"../data/tcr_{scheme}_expected_1.txt")
+    model.to_json(f"../data/expected_data/tcr_{scheme}_expected_1.json")
+    model.to_text(f"../data/expected_data/tcr_{scheme}_expected_1.txt")
